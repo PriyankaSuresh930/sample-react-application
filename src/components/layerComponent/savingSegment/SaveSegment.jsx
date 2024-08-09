9b7440dfefcbac68f8b6017a362d1d71fc46f61a
PriyankaSuresh930 <124255449+PriyankaSuresh930@users.noreply.github.com>
2024-08-09 17:52:52 +0530

    Update SaveSegment.jsx

--- src/components/layerComponent/savingSegment/SaveSegment.jsx
+++ src/components/layerComponent/savingSegment/SaveSegment.jsx
@@ -4,7 +4,7 @@ const SaveSegment = ({handleSubmitSegment}) => {
   return (
     <div className='saving-segment-footer'>
       <button type='submit' className="save-segment-btn me-3" onClick={handleSubmitSegment}>Save the Segment</button>
-      <button type='button' className='cancel-btn'>Cancel</button>
+      <button type='button' className='cancel-btn' data-bs-dismiss="offcanvas" aria-label="Close">Cancel</button>
     </div>
   )
 }
